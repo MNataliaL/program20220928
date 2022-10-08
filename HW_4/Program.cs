cb3d28acb172b0ceac11336355aa2959cab626dd
Natalya <mokhovikova.n@bk.ru>
2022-10-09 00:59:55 +0800

    Макс и мин из трех чисел

---
+++ HW_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a, min = b;
+
+if(max < min)
+{
+max = b;
+min = a;
+}
+
+if(max < c) max = c;
+if(min > c) min = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
